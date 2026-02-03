91ad28b8b8d78025daf5eba2e24d2c114a7f00c2
Lin, Ryan <wrian007w@gmail.com>
2026-02-03 13:17:28 -0800

    try new 'ryan' component

--- blocks/ryan/ryan.js
+++ blocks/ryan/ryan.js
@@ -1,5 +1,3 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-
 export default function decorate(block) {
   const para = document.createElement("p");
   para.innerText = "This is a paragraph";
