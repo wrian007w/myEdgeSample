782d479f9cd481742d496a12a60afed76874f44a
Lin, Ryan <wrian007w@gmail.com>
2026-02-03 13:19:51 -0800

    try new 'ryan' component

--- blocks/ryan/ryan.js
+++ blocks/ryan/ryan.js
@@ -1,6 +1,6 @@
 export default function decorate(block) {
-  const para = document.createElement("p");
-  para.innerText = "This is a paragraph";
+  const para = document.createElement('p');
+  para.innerText = 'This is a paragraph';
 
   block.append(para);
 }
